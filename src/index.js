f9b6ff137ee6194d807e038d0e638568be245acc
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-06-25 23:14:23 +0200

    try callback save

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const xml = require("xmlhttprequest").XMLHttpRequest;
 
 const config = JSON.stringify(fs.readFileSync(`${__dirname}/config.json`).toString());
+var stor = fs.readFileSync(`${__dirname}/stor.json`);
 
 const steamAPI = `http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=${config.key}&format=json`;
 var out = new Array(),
@@ -36,8 +37,7 @@ function initRequest(){
             list = new Array();
 
             reques(numb, tempnumb, list);
-            
-        //        fs.writeFileSync('./stor.json', gameList);
+
         }else{
             if(this.readyState == 4){
                 console.log(">"+ this.status);
@@ -62,16 +62,34 @@ function reques(numb, tempnumb, list){
                         if(req.readyState == 4 && req.status == 200){
 
                             const content = JSON.parse(req.responseText);
-                            out.push(content);
+
+                            for(var [id, item] of Object.entries(content)){
+                                if(item.data && item.data.price_overview){
+                                    if(item.data.price_overview.discount_percent == 100){
+                                        
+                                        var title = await checkName(id);
+                                        console.log("Title: "+ title);
+
+                                        const link = `https://store.steampowered.com/app/${id}/${title}/`
+                                        
+                                        var array = new Array(title, item);
+                                        stor.push(array);
+                                    }
+                                }
+                            }
+
 
                             reques(numb, tempnumb, list);                            
                         }else{
                             if(req.readyState == 4){
+                                console.log(`Request Faild \nerr(${req.status}). try to handle`);
+
                                 //skip the 500 error bug. have to fix. its PFUSCH we would say in germany
                                 if(req.status == 500){
+                                    console.log("Skiped");
                                     reques(numb, tempnumb, list);
                                 }else{
-                                    console.log(`err(${req.status}). try to catch`);
+                                    console.log("try again");
 
                                     list = list.split(",");
                                     await list.forEach(e => {
@@ -86,5 +104,28 @@ function reques(numb, tempnumb, list){
                     }
                 }else{
                     console.log(out);
-                }    
+                }
+}
+function checkName(id){
+    console.log(`fetch name for ${id}`);
+    var checkreq = new xml();
+    checkreq.open('GET',  `https://store.steampowered.com/api/appdetails?appids=${id}`, true);
+    checkreq.send();
+    checkreq.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+            if(response[id].data && response[id].data.name){
+                console.log(">"+ response[id].data.name);
+                return response[id].data.name;
+            }else{
+                console.log("data error");
+                setTimeout(checkName, 120000, id);
+            }
+        }
+        else if(this.readyState == 4){
+            console.log(`Fetch Faild \nerr(${this.status})`);
+            setTimeout(checkName, 120000, id);
+        }
+    }
 }
