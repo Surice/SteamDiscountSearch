aaa5ee97a37f1deed1db5c382853cf9b0ae4f370
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-06-28 09:54:12 +0200

    push output wokring.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const xml = require("xmlhttprequest").XMLHttpRequest;
 
-const config = JSON.stringify(fs.readFileSync(`${__dirname}/config.json`).toString());
-var stor = fs.readFileSync(`${__dirname}/stor.json`);
+const config = JSON.stringify(fs.readFileSync(`${__dirname}/config.json`));
+//var stor = fs.readFileSync(`${__dirname}/stor.json`);
+
+var stor = new Array();
 
 const steamAPI = `http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=${config.key}&format=json`;
-var out = new Array(),
-    ids = new Array(),
+var ids = new Array(),
     count = 0,
-    state = -1,
-    cache = 0;
+    state = 0,
+    temp = 10;
 
 initRequest();
 
@@ -50,6 +51,7 @@ function reques(numb, tempnumb, list){
                 if(numb > 0){
                     list = ids.splice(0, 1000);
                     numb --;
+                    state++;
                                         
                     list = list.join(",");
 
@@ -66,20 +68,20 @@ function reques(numb, tempnumb, list){
                             for(var [id, item] of Object.entries(content)){
                                 if(item.data && item.data.price_overview){
                                     if(item.data.price_overview.discount_percent == 100){
-                                        
-                                        var title = await checkName(id);
-                                        console.log("Title: "+ title);
 
-                                        const link = `https://store.steampowered.com/app/${id}/${title}/`
-                                        
-                                        var array = new Array(title, item);
-                                        stor.push(array);
+                                        checkName(id, function(result){
+                                            console.log("result: "+ result);
+
+                                            const link = `https://store.steampowered.com/app/${id}/${result}/`
+
+                                            var push = result+ " | "+ link.toString();
+                                            stor.push(push);
+                                            fs.writeFileSync(`${__dirname}/stor.json`, stor);
+                                        });
                                     }
                                 }
                             }
-
-
-                            reques(numb, tempnumb, list);                            
+//                            reques(numb, tempnumb, list);
                         }else{
                             if(req.readyState == 4){
                                 console.log(`Request Faild \nerr(${req.status}). try to handle`);
@@ -87,7 +89,7 @@ function reques(numb, tempnumb, list){
                                 //skip the 500 error bug. have to fix. its PFUSCH we would say in germany
                                 if(req.status == 500){
                                     console.log("Skiped");
-                                    reques(numb, tempnumb, list);
+//                                    reques(numb, tempnumb, list);
                                 }else{
                                     console.log("try again");
 
@@ -102,11 +104,15 @@ function reques(numb, tempnumb, list){
                             }
                         }
                     }
-                }else{
-                    console.log(out);
+                    if(state != temp){
+                        reques(numb, tempnumb, list);
+                    }else{
+                        temp == temp + 10;
+                        setTimeout(reques, 20000,numb, tempnumb, list);
+                    }
                 }
 }
-function checkName(id){
+function checkName(id, callback){
     console.log(`fetch name for ${id}`);
     var checkreq = new xml();
     checkreq.open('GET',  `https://store.steampowered.com/api/appdetails?appids=${id}`, true);
@@ -117,15 +123,15 @@ function checkName(id){
             console.log(response);
             if(response[id].data && response[id].data.name){
                 console.log(">"+ response[id].data.name);
-                return response[id].data.name;
+                callback(response[id].data.name);
             }else{
                 console.log("data error");
-                setTimeout(checkName, 120000, id);
+                setTimeout(checkName, 120000, id, callback);
             }
         }
         else if(this.readyState == 4){
             console.log(`Fetch Faild \nerr(${this.status})`);
-            setTimeout(checkName, 120000, id);
+            setTimeout(checkName, 120000, id, callback);
         }
     }
 }
